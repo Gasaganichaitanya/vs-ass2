99e6eb5741bd4cdf15c02f58400b52b0e33a9fcb
Gsaganichaitanya <chaitanyagasagani@gmail.com>
2023-11-08 15:09:52 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsApp_Ass2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int id;
+            string employeename;
+            string employeegender;
+            double employeesalary;
+            DateTime doj;
+            Console.WriteLine("Enter id");
+            id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Employee name");
+            employeename = (Console.ReadLine());
+            Console.WriteLine("Enter employee Gender");
+            employeegender = (Console.ReadLine());
+            Console.WriteLine("Enter Employee Salary");
+            employeesalary = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter DOJ (D/M/Y)");
+            doj = DateTime.Parse(Console.ReadLine());
+            
+            Console.WriteLine("\nID\tEmployeeName\tEmployeegender\tEmployeeSalary\tDOJ");
+            Console.WriteLine("__________________________________________________________");
+            Console.WriteLine($"{id}\t{employeename}\t{employeegender}\t{employeesalary}\t{doj}");
+            if (employeesalary > 90000)
+            {
+                Console.WriteLine("you have to pay 30%tax");
+            }
+            else
+            {
+                Console.WriteLine("you have to pay 15% tax");
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
